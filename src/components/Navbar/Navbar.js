37f517c908003a20c0a074dc282a410dd9cf5b1f
JeramDev <jesusramosdev@gmail.com>
2019-09-26 19:13:53 +0100

    Prueba con Semantic ui react - X

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+import './Navbar.css';
+
+export default class Navbar extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Segment inverted>
+        <Menu inverted pointing secondary>
+        <Menu.Item className="btn1"
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='messages'
+            active={activeItem === 'messages'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='friends'
+            active={activeItem === 'friends'}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+      </Segment>
+    )
+  }
+}
